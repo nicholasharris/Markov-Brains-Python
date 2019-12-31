ffb4649ad357c6b852e805f7240767348c88dd83
Nicholas Harris <nicholassharris42@gmail.com>
2019-12-31 11:50:38 -0800

    Update markov.py

--- markov.py
+++ markov.py
@@ -1,5 +1,5 @@
 ##################################
-# A (probably flawed) implementation of Markov brains,
+# An implementation of Markov brains,
 # Based on article: http://devosoft.org/a-quick-introduction-to-markov-network-brains/
 #   from devolab of Michigan State University
 #
